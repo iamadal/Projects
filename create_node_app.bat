f5bf8d6f77ee2a8aeb71b9d2601a7d6b8c6b9c1b
iamadal <mdadalkhan@gmail.com>
2024-11-24 14:42:25 +0600

    Added ts-node

---
+++ create_node_app.bat
@@ -0,0 +1,16 @@
+mkdir biam_cpf_node
+cd biam_cpf_node
+
+npm init -y
+
+npm install --save-dev typescript ts-node
+npm install --save-dev @types/express  
+tsc --init
+mkdir src
+npx tsc
+
+
+
+
+
+

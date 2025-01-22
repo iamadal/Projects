08a1ec19509edf299ea6e8871d77220445e09c5c
iamadal <mdadalkhan@gmail.com>
2025-01-22 13:59:57 +0600

    Up

--- node/app.js
+++ node/app.js
@@ -1,49 +1,92 @@
-// Import
-const express    = require('express');
-const helmet     = require('helmet');
-const cors       = require('cors');
-const rateLimit  = require('express-rate-limit');
-const path       = require('path');
-const dotenv     = require('dotenv');
-const web        = require('./src/actions.js')
-const route      = require('./src/routes.js')
+/**
+ * @author Adal Khan
+ * import require modules.
+ * */
+
+const express           = require('express')
+const helmet            = require('helmet')
+const cors              = require('cors')
+const rateLimit         = require('express-rate-limit');
+const path              = require('path')
+const dotenv            = require('dotenv')
+const mongoose          = require('mongoose')
+const cookieParser      = require('cookie-parser')
+const csrf              = require('csurf')
+const passport          = require('passport')
+const session           = require('express-session')
+const bcrypt            = require('bcryptjs')
+const {body,validation} = require('express-validator')
+const crypto            = require('crypto')
+const web               = require('./src/actions.js')
+const route             = require('./src/routes.js')
+const m_users           = require('./src/data/m_users.js')
+const m_items           = require('./src/data/m_items.js')
 
 
-dotenv.config();
-const app = express();
+/**
+ * Configurations.
+ * */
+const corsOptions    = { origin: 'http://localhost:3000', methods: ['GET', 'POST']};
+const limiter        = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+const csrfProtection = csrf({cookie: true})
+const sessionSecret  = crypto.randomBytes(32).toString('hex'); 
+const app            = express()
 
-
-// Options
-const corsOptions = { origin: 'http://localhost:3000', methods: ['GET', 'POST']};
-const limiter     = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
-
-// Middlewares
-app.use(helmet());
-app.use(cors(corsOptions));
-app.use(limiter);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// View Engine
+/**
+ * Template Engine(EJS) configuration.
+ * */
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'src','views'));
 
 
-// Front Controller ---------------------------------------------------------------------------------
+/**
+ * Middlewares
+ * */
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(limiter)
+app.use(cookieParser())
+app.use(session({secret: sessionSecret, resave: false, saveUninitialized: true }))
+app.use(csrfProtection)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+/**
+ * Database Connection
+ * */
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/biam_app_data')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+
+
+
+
+
+
+/**
+ * Route Maps
+ * */
 
 app.get(route.index ,  web.index)
 app.get(route.about ,  web.about)
 
-//---------------------------------------------------------------------------------------------------
 
-// Error Handler
+/* Global Error Handler */
 
 app.use((req, res) => {
   res.render('error');
 });
 
-// Serve
+
+/* Server Configurations */
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
